ad63eb3e22c29bfd51163a2ba79c41a36dc22403
seanhelvey <seanhelvey@Seans-MacBook-Pro-2.local>
2018-09-14 14:43:56 -0600

    port from spark to ktor

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,34 @@
-import spark.kotlin.*
+package blog
 
-//Note: Runs on http://localhost:4567/hello
+import com.sun.xml.internal.ws.developer.SerializationFeature
+import io.ktor.application.*
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.http.*
+import io.ktor.jackson.jackson
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import java.lang.Compiler.enable
+
+fun Application.module() {
+    install(DefaultHeaders)
+    install(CallLogging)
+    install(ContentNegotiation) {
+        jackson {
+
+        }
+    }
+    install(Routing) {
+        get("/snippets") {
+            call.respond(kotlin.collections.mapOf("OK" to true))
+        }
+    }
+}
 
 fun main(args: Array<String>) {
-    val http: Http = ignite().port((System.getenv("PORT") ?: "8080").toInt())
-    http.get("/hello") {
-        "Hello Spark Kotlin!"
-    }
+    val port = (System.getenv("PORT") ?: "8080").toInt()
+    embeddedServer(Netty, port, watchPaths = listOf("BlogAppKt"), module = Application::module).start()
 }
