56dcf2bbb514608657af377ea1b71fb7e8eadba3
seanhelvey <seanhelvey@Seans-MacBook-Pro-2.local>
2018-09-14 11:10:31 -0600

    set port

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,8 +3,7 @@ import spark.kotlin.*
 //Note: Runs on http://localhost:4567/hello
 
 fun main(args: Array<String>) {
-    val http: Http = ignite()
-
+    val http: Http = ignite().port((System.getenv("PORT") ?: "8080").toInt())
     http.get("/hello") {
         "Hello Spark Kotlin!"
     }
