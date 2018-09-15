d6e761d9bb0d510cf7c0b2688f46615a09be69a0
seanhelvey <sean.helvey@gmail.com>
2018-09-15 08:21:12 -0600

    gitignore, readme, default route

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # kotlinplayground
+
+A Kotlin back-end built with the [Ktor](http://ktor.io/) framework.
+
+Notes on deployment:
+- [Ktor Gradle setup](http://ktor.io/quickstart/index.html#gradle-setup)
+- [Kotlin on Heroku](https://devcenter.heroku.com/articles/getting-started-with-kotlin#define-a-procfile)
+- [Gradle build for Heroku](https://devcenter.heroku.com/articles/deploying-gradle-apps-on-heroku#verify-that-your-build-file-is-set-up-correctly)
+- [Gradle fat jar](https://www.baeldung.com/gradle-fat-jar)
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,15 +1,12 @@
-import com.sun.xml.internal.ws.developer.SerializationFeature
 import io.ktor.application.*
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
-import io.ktor.http.*
 import io.ktor.jackson.jackson
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
-import java.lang.Compiler.enable
 
 fun Application.module() {
     install(DefaultHeaders)
@@ -20,6 +17,9 @@ fun Application.module() {
         }
     }
     install(Routing) {
+        get("/") {
+            call.respondText("API at /snippets")
+        }
         get("/snippets") {
             call.respond(kotlin.collections.mapOf("OK" to true))
         }
