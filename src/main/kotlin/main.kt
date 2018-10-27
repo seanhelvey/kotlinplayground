a66c9e014ab5d74604695942abc4ee87a7dc645c
seanhelvey <sean.helvey@gmail.com>
2018-10-27 15:05:31 -0600

    POST snippet working

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,10 +3,23 @@ import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.jackson.jackson
+import io.ktor.request.receive
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import java.util.*
+
+data class Snippet(val text: String)
+
+data class PostSnippet(val snippet: PostSnippet.Text) {
+    data class Text(val text: String)
+}
+
+val snippets = Collections.synchronizedList(mutableListOf(
+    Snippet("hello"),
+    Snippet("world")
+))
 
 fun Application.module() {
     install(DefaultHeaders)
@@ -22,6 +35,11 @@ fun Application.module() {
         }
         get("/snippets") {
             call.respond(kotlin.collections.mapOf("OK" to true))
+        }
+        post("/snippets") {
+            val post = call.receive<PostSnippet>()
+            snippets += Snippet(post.snippet.text)
+            call.respond(mapOf("OK" to true))
         }
     }
 }
